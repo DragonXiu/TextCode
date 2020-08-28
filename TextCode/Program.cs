72c611464e8f025d3be4bf1a8d95acbf05053403
方 徐 <15066253576@163.com>
2020-08-28 13:48:58 +0800

    1

--- TextCode/Program.cs
+++ TextCode/Program.cs
@@ -1356,8 +1356,8 @@ namespace TextCode
 
             return list;
         }
-        #endregion
-        #region 只出现一次的数字
+        #endregion              
+         #region 只出现一次的数字
         public int SingleNumber(int[] nums)
         {
             int num = 0;
@@ -1395,8 +1395,91 @@ namespace TextCode
             //return 0;
         }
         #endregion
+        #region 环形链表
+        public bool hasCycle(ListNode head)
+        {
+            #region 哈希表
 
-    }
+
+            //HashSet<ListNode> nodes = new HashSet<ListNode>();
+            //while (head!=null)
+            //{
+            //    if (nodes.Contains(head))
+            //    {
+            //        return true;
+            //    }
+            //    else
+            //    {
+            //        nodes.Add(head);
+            //    }
+            //    head=head.next;
+            //}
+            //return false;
+            #endregion
+            #region 双指针
+            /*通过使用具有 不同速度 的快、慢两个指针遍历链表，空间复杂度可以被降低至 
+             * O(1)O(1)O(1)。慢指针每次移动一步，而快指针每次移动两步。如果列表中不
+             * 存在环，最终快指针将会最先到达尾部，此时我们可以返回 false。现在考虑一
+             * 个环形链表，把慢指针和快指针想象成两个在环形赛道上跑步的运动员  
+             * （分别称之为慢跑者与快跑者）。而快跑者最终一定会追上慢跑者。这是为什么呢？
+             * 考虑下面这种情况（记作情况 A）- 假如快跑者只落后慢跑者一步，在下一次迭代中，
+             * 它们就会分别跑了一步或两步并相遇。其他情况又会怎样呢？例如，我们没有考虑快
+             * 跑者在慢跑者之后两步或三步的情况。 其实不难想到，因为在下一次或者下下次迭代后
+             * ，又会变成上面提到的情况 A。
+*/
+            if (head == null||head.next==null)
+            {
+                return false;
+            }
+            ListNode slow = head;
+            ListNode fast = head.next;
+            while (slow!=fast)
+            {
+                if (fast==null||fast.next ==null)
+                {
+                    return false;
+                }
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return true;
+            #endregion
+        }
+        #endregion
+        #region 机器人是否回到原点
+        public bool JudgeCircle(string moves)
+        {
+            //垂直
+            int vertical = 0;
+            //水平
+            int level = 0;
+            int i = 0;
+            while (i<moves.Length)
+            {
+                switch (moves[i])
+                {
+                    case 'U':
+                        vertical++;
+                        break;
+                    case 'L':
+                        level++;
+                        break;
+                    case 'D':
+                        vertical--;
+                        break;
+                    case 'R':
+                        level--;
+                        break;
+                }
+                i++;
+            }
+            return vertical==0&&level==0;
+        }
+   
+
+    #endregion
+ }
+
     public class ListNode
     {
         public int val;
@@ -1407,5 +1490,6 @@ namespace TextCode
         }
     }
 }
+
 
 
