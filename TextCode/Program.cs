349b6a9163e1586b9fb4be1e46c523c2103a402c
方 徐 <15066253576@163.com>
2020-11-21 10:39:41 +0800

    121

--- TextCode/Program.cs
+++ TextCode/Program.cs
@@ -6552,6 +6552,67 @@ new int[] {-2,2}}, 1);
             return Math.Abs(r1-r2)+Math.Abs(c1-c2);
         }
         #endregion
+        #region 排序链表
+        public ListNode SortList(ListNode head)
+        {
+            return SortList(head,null);
+        }
+        public ListNode SortList(ListNode head,ListNode tail)
+        {
+            if (head==null)
+            {
+                return head;
+            }
+            if (head.next==tail)
+            {
+                head.next = null;
+                return head;
+            }
+            ListNode slow = head, fast = head;
+            while (fast!=tail)
+            {
+                slow = slow.next;
+                fast = fast.next;
+                if (fast!=tail)
+                {
+                    fast = fast.next;
+                }
+            }
+            ListNode mid = slow;
+            ListNode list1 = SortList(head,mid);
+            ListNode list2 = SortList(mid,tail);
+            ListNode sorted = Merge(list1, list2);
+            return sorted;
+        }
+        public ListNode Merge(ListNode head1, ListNode head2)
+        {
+            ListNode dummyHead = new ListNode(0);
+            ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+            while (temp1!=null&&temp2!=null)
+            {
+                if (temp1.val<=temp2.val)
+                {
+                    temp.next = temp1;
+                    temp1 = temp1.next;
+                }
+                else
+                {
+                    temp.next = temp2;
+                    temp2 = temp2.next;
+                }
+                temp = temp.next;
+            }
+            if (temp1!=null)
+            {
+                temp.next = temp1;
+            }
+            else if (temp2!=null)
+            {
+                temp.next = temp2;
+            }
+            return dummyHead.next;
+        }
+        #endregion
         #endregion
         #region LinQ
         private static void DataInit()
