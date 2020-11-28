d58043c4442b47a28abff06e511da3b9be06ea66
方 徐 <15066253576@163.com>
2020-11-28 10:14:40 +0800

    1

--- TextCode/Program.cs
+++ TextCode/Program.cs
@@ -443,6 +443,7 @@ new int[] {-2,2}}, 1);
             SortByBits(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 });
             FindMinArrowShots(new int[][] { new int[] { 2, 6 }, new int[] { 2, 8 }, new int[] { 10, 16 }, new int[] { 7, 12 } });
             // FindRotateSteps("nyngl","yyynnnnnnlllggg");
+            ReversePairs(new int[] { 2147483647, 2147483647, 2147483647, 2147483647, 2147483647, 2147483647 });
             CheckRecord("PPALLLPL");
             Console.ReadLine();
         }
@@ -6707,6 +6708,76 @@ new int[] {-2,2}}, 1);
             }
             return ret.ToString();
         }
+        #endregion
+        #region 翻转对
+        public static int ReversePairs(int[] nums)
+        {
+            
+            if (nums.Length==0)
+            {
+                //没有元素，返回0
+                return 0;
+            }
+            return mergSort(nums,0,nums.Length-1);
+        }
+        //对当前的序列进行归并排序
+        private static int mergSort(int[] nums,int start,int end)
+        {
+            if (start==end)
+            {
+                //不在构成序列，返回递归的出口
+                return 0;
+            }
+            int mid = (start + end)/2 ;//中点
+            int n1 = mergSort(nums,start,mid);//左序列归并排序
+            int n2 = mergSort(nums,mid+1,end);//右序列归并排序
+            int ret = n1 + n2;
+            //此时左右序列都已经排序，现在合并，以及合并前的统计
+            int i = start;//i指向左序列的开头
+            int j = mid + 1;//j指向有序列的开头
+            while (i<=mid)
+            {
+                while (j<=end&&(long)nums[i]>2*(long)nums[j])
+                {
+                    j++;
+                }
+                ret += j - mid - 1;
+                i++;
+            }
+            //合并两个排序数组
+            int[] sortnum = new int[end-start + 1];
+            int p1 = start, p2 = mid + 1;
+            int p = 0;
+            while (p1<=mid||p2<=end)
+            {
+                if (p1>mid)
+                {
+                    sortnum[p++] = nums[p2++];
+                }else if (p2 > end)
+                {
+                    sortnum[p++] = nums[p1++];
+                }
+                else
+                {
+                    if (nums[p1]<nums[p2])
+                    {
+                        sortnum[p++] = nums[p1++];
+                    }
+                    else
+                    {
+                        sortnum[p++] = nums[p2++];
+                    }
+                }
+            }
+            for (int n = 0; n < sortnum.Length; n++)
+            {
+                nums[start + n] = sortnum[n];
+            }
+            return ret;
+
+
+        }
+
         #endregion
         #endregion
         #region LinQ
