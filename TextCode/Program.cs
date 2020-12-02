ee63f81ce399e60a45e3a873c267dbb93845d196
方 徐 <15066253576@163.com>
2020-12-02 09:48:33 +0800

    1

--- TextCode/Program.cs
+++ TextCode/Program.cs
@@ -6911,6 +6911,94 @@ new int[] {-2,2}}, 1);
 
         }
         #endregion
+        #region 拼接最大数
+        public int[] MaxNumber(int[] nums1, int[] nums2, int k)
+        {
+            int[] res = new int[k];
+            int n1 = nums1.Length;
+            int n2 = nums2.Length;
+            int start = Math.Max(0, k - n2), end = Math.Min(k,n1);
+            for (int i = start; i <=end; i++)
+            {
+                int[] sub1 = Pick_max(nums1,i);
+                int[] sub2 = Pick_max(nums2,k-i);
+                int[] cur = Merge(sub1,sub2);
+                if (Compare(cur,res,0,0)>0)
+                {
+                    Array.Copy(cur,0,res,0,k);
+                }
+            }
+            return res;
+        }
+        private int[] Pick_max(int[] nums,int k)
+        {
+            int len = nums.Length;
+            int[] stack = new int[k];
+            int top = -1;
+            int remain = len - k;
+            for (int i = 0; i < len; i++)
+            {
+                int num = nums[i];
+                while (top>=0&&stack[top]<num&&remain>0)
+                {
+                    top--;
+                    remain--;
+                }
+                if (top<k-1)
+                {
+                    stack[++top] = num;
+                }
+                else
+                {
+                    remain--;
+                }
+            }
+            return stack;
+        }
+        private int[] Merge(int[] sub1, int[] sub2)
+        {
+            int a = sub1.Length, b = sub2.Length;
+            if (a==0)
+            {
+                return sub2;
+            }
+            if (b==0)
+            {
+                return sub1;
+            }
+            int merglen = a + b;
+            int[] m = new int[merglen];
+            int n1 = 0, n2 = 0;
+            for (int i = 0; i < merglen; i++)
+            {
+                if (Compare(sub1,sub2,n1,n2)>0)
+                {
+                    m[i] = sub1[n1++];
+                }
+                else
+                {
+                    m[i] = sub2[n2++];
+                }
+            }
+            return m;
+        }
+        private int Compare(int []sub1,int[]sub2,int n1,int n2) {
+            int a = sub1.Length, b = sub2.Length;
+            while (n1<a&&n2<b)
+            {
+                int dif = sub1[n1] - sub2[n2];
+                if (dif!=0)
+                {
+                    return dif;
+                }
+                n1++;
+                n2++;
+            }
+            return (a - n1) - (b - n2);
+        }
+
+
+        #endregion
         #endregion
         #region LinQ
         private static void DataInit()
