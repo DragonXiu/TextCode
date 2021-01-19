a9785a7d8faa903eabfc9a916e05faf22fc65311
方 徐 <15066253576@163.com>
2021-01-19 10:10:53 +0800

    1

--- TextCode/Program.cs
+++ TextCode/Program.cs
@@ -9446,6 +9446,43 @@ new int[] {-2,2}}, 1);
         }
         #endregion
 
+        #region 连接所有点最小值
+        public int MinCostConnectPoints(int[][] points)
+        {
+            if (points.Length <= 1)
+            {
+                return 0;
+            }
+            var dictionary = new Dictionary<(int, int), int>();
+            for (var i = 0; i < points.Length; i++)
+            {
+                for (int j = i + 1; j < points.Length; j++)
+                {
+                    dictionary[(i, j)] = Math.Abs(points[i][0] - points[j][0]) + Math.Abs(points[i][1] - points[j][1]);
+                }
+            }
+
+            var pairs = dictionary.OrderBy(it => it.Value).ToList();
+
+            HashSet<int> set = new HashSet<int>() { pairs[0].Key.Item1 };
+            var sum = 0;
+            while (set.Count < points.Length)
+            {
+                foreach (var pair in pairs)
+                {
+                    if ((set.Contains(pair.Key.Item1) && !set.Contains(pair.Key.Item2))
+                        || (set.Contains(pair.Key.Item2) && !set.Contains(pair.Key.Item1)))
+                    {
+                        sum += pair.Value;
+                        set.Add(pair.Key.Item1);
+                        set.Add(pair.Key.Item2);
+                        break;
+                    }
+                }
+            }
+            return sum;
+        }
+        #endregion
 
         #endregion
         #region LinQ
@@ -9544,7 +9581,7 @@ new int[] {-2,2}}, 1);
     }
     class Employee1
     {
-        public int id;
+        public int id;  
         public int importance;
         public IList<int> subordinates;
     }
